77abb3d2ba5479006dce136765aace4642299b30
Yakshit Crio.Do <yakshit@criodo.com>
2022-11-20 17:14:33 +0550

    updated changes - 2022-11-20 16:54:33

--- app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
+++ app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
-import com.crio.qcalc.StandardCalculator;
-
 public class StandardCalculatorTest {
     private StandardCalculator standardCalculator;
 
