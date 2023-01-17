a5f3b38b98ffb24b3c000313784aa84dad43e742
CrioUser <crio-user@criodo.com>
2023-01-17 17:09:32 +0530

    debug and exception handling

--- app/src/main/java/com/crio/qcalc/StandardCalculator.java
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -5,7 +5,7 @@ public class StandardCalculator {
     public static void getVersion(){
         System.out.println("Standard Calculator 1.0");
     }
-    private double result;
+    protected double result;
 
     public double getResult() {
         return result;
@@ -24,44 +24,62 @@ public class StandardCalculator {
     public void printResult(){
         System.out.println("Standard Calculator Result:"+ result);
     }
-    
-    public  void add(int num1, int num2){
+
+    public final void add(int num1, int num2){
         add((double)num1, (double)num2);
         
     }
     
-    public  void subtract(int num1, int num2){
+    public final void subtract(int num1, int num2){
         subtract((double)num1, (double)num2);
     }
 
-    public void multiply(int num1, int num2){
+    public final void multiply(int num1, int num2){
         multiply((double)num1, (double)num2);
     }
 
-    public void divide(int num1, int num2){
+    public final void divide(int num1, int num2){
         divide((double)num1, (double)num2);
     }
     
-
-    public void subtract(double num1, double num2){
-        result =  num1-num2;
+    public final void subtract(double num1, double num2){
+        //result =  num1-num2;
+        double result = num1 - num2;
+        if((result == -Double.MAX_VALUE) || (result == Double.NEGATIVE_INFINITY)){
+            throw new ArithmeticException("Double overflow");
         }
-
-
-    public void multiply(double num1, double num2){
-        result = num1*num2;        
+        this.result = result;
     }
 
-    public void divide(double num1, double num2){
+
+    public final void multiply(double num1, double num2){
+        //result = num1*num2;
+        double result = num1 * num2;
+        if((result == Double.MAX_VALUE) ||(result == -Double.MAX_VALUE) || (result == Double.NEGATIVE_INFINITY) || (result == Double.POSITIVE_INFINITY)){
+            throw new ArithmeticException("Double overflow");
+        }
+        this.result = result;
+    }
+
+
+    public final void divide(double num1, double num2){
         //result = num1/num2;
-        result = num1 / num2;
+        double result = num1 / num2;
+        if(num2 == 0.0){
+
+            throw new ArithmeticException("Divide By Zero");
+
+        }
+        this.result = result;
     }
 
     
-    public void add(double num1, double num2){
-        result = num1 + num2;
+    public final void add(double num1, double num2){
+        double result = num1 + num2;
+        if((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)){
+            throw new ArithmeticException("Double overflow");
+        }
+        this.result = result;
     }
-
-
 
 }
--- app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
+++ app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
@@ -1,133 +1,118 @@
-
 package com.crio.qcalc;
 
-
 import org.junit.jupiter.api.Assertions;
-
 import org.junit.jupiter.api.BeforeEach;
-
 import org.junit.jupiter.api.DisplayName;
-
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.function.Executable;
 
 public class StandardCalculatorTest {
-
     private StandardCalculator standardCalculator;
 
-
     @BeforeEach
-
     void setup(){
-
         standardCalculator = new StandardCalculator();
-
     }
- //add   
-@Test
 
-@DisplayName("Test Addition of Two Integers")
 
-void testAdditionOperation(){
+    @Test
+    @DisplayName("Test Addition Overflow of Two Doubles")
+    void testAdditionOverflowForDoubles(){
+        //Assert
+        Assertions.assertThrows(ArithmeticException.class,new Executable(){
+            @Override
+            public void execute() throws Throwable{
+                standardCalculator.add(Double.MAX_VALUE, Double.MAX_VALUE);
+            }
+        });
+    }
+    @Test
+    @DisplayName("Test Subtraction Overflow of Two Doubles")
+    void testSubtractionOverflowForDoubles(){
+        //Assert
+        Assertions.assertThrows(ArithmeticException.class,new Executable(){
+            @Override
+            public void execute() throws Throwable{
+                standardCalculator.subtract(-Double.MAX_VALUE,Double.MAX_VALUE);
+            }
+        });
+    }
 
-        standardCalculator.add(1,1);
-
-        double actualResult = standardCalculator.getResult();
-
-        Assertions.assertEquals(2, actualResult);
-
-}
-//subtract
-
-@Test
-
-@DisplayName("Test Subtraction of Two Integers")
-
-void testSubtractionOperation(){
-
-        standardCalculator.subtract(1,1);
-
-        double actualResult = standardCalculator.getResult();
-
-        Assertions.assertEquals(0, actualResult);
-
-}
-
-//multiply
-@Test
-
-@DisplayName("Test Multiplication of Two Integers")
-
-void testMultiplicationOperation (){
-
-        standardCalculator.multiply(1,1);
-
-        double actualResult = standardCalculator.getResult();
-
-        Assertions.assertEquals(1, actualResult);
-}
-//divide
-@Test
-
-@DisplayName("Test Division of Two Integers")
-
-void testDivisionOperation (){
-
-        standardCalculator.divide(1,1);
-
-        double actualResult = standardCalculator.getResult();
-
-        Assertions.assertEquals(1, actualResult);
-}
-@Test
-
-@DisplayName("Test Addition of Two Doubles")
-
-void testAdditionOperationForDoubles(){
-
+    @Test
+    @DisplayName("Test Addition of Two Doubles")
+    void testAdditionOperationForDoubles(){
         standardCalculator.add(1.0,1.5);
-
         double actualResult = standardCalculator.getResult();
-
         Assertions.assertEquals(2.5, actualResult);
+    }
 
-}
-
-
-@Test
-
-@DisplayName("Test Subtraction of Two Doubles")
-
-void testSubtractionOperationForDoubles(){
-
+    @Test
+    @DisplayName("Test Substraction of Two Doubles")
+    void testSubtractionOperationForDoubles(){
         standardCalculator.subtract(10.0,20.5);
-
         double actualResult = standardCalculator.getResult();
-
         Assertions.assertEquals(-10.5, actualResult);
-
-}
-
-@Test
-
-@DisplayName("Test Multiplication of Two Doubles")
-
-void testMultiplicationOperationForDoubles(){
+    }
+    
+    @Test
+    @DisplayName("Test Multiplication of Two Doubles")
+    void testMultiplicationOperationForDoubles(){
         standardCalculator.multiply(2.0,2.0);
         double actualResult = standardCalculator.getResult();
         Assertions.assertEquals(4.0, actualResult);
-}
+    }
 
-@Test
-@DisplayName("Test Division of Two Doubles")
-void testDivisionOperationForDoubles(){
-    standardCalculator.divide(10.0,20.0);
-    double actualResult = standardCalculator.getResult();
-    Assertions.assertEquals(0.5, actualResult);
-}
 
+    @Test
+    @DisplayName("Test Division of Two Doubles")
+    void testDivisionOperationForDoubles(){
+        standardCalculator.divide(10.0,20.0);
+        double actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(0.5, actualResult);
+    }
+
+
+
+
+    @Test
+    @DisplayName("Test Addition of Two Integers")
+    void testAdditionOperation(){
+        standardCalculator.add(1,1);
+        double actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(2, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Substraction of Two Integers")
+    void testSubtractionOperation(){
+        standardCalculator.subtract(1,1);
+        double actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(0, actualResult);
+    }
 
     
+    @Test
+    @DisplayName("Test Multiplication of Two Integers")
+    void testMultiplicationOperation(){
+        standardCalculator.multiply(2,1);
+        double actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(2, actualResult);
+    }
+
+
+    @Test
+    @DisplayName("Test Division of Two Integers")
+    void testDivisionOperation(){
+        standardCalculator.divide(4,2);
+        double actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(2, actualResult);
+    }
+    
+  
+
+
+
+
+
 
 }
-
