cc1c72eb103a37be1e364063ba5ce50c4e6ed119
CrioUser <crio-user@criodo.com>
2023-01-17 10:27:00 +0530

    test

--- app/src/main/java/com/crio/qcalc/App.java
+++ app/src/main/java/com/crio/qcalc/App.java
@@ -11,13 +11,13 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         StandardCalculator calc = new StandardCalculator();
-         calc.add(1, 2);
-         System.out.println(calc.getResult());
+        calc.add(1, 2);
+        System.out.println(calc.getResult());
 
         
         //LogicCalculator calc = new LogicCalculator();
-       // calc.OR(8, 6);
-       // calc.printResult();
+        //calc.OR(8, 6);
+        //calc.printResult();
 
     }
 }
