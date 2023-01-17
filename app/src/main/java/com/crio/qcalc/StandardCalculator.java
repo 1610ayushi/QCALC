8e653537ee76142dbc974a5e28c0746dd5070f32
CrioUser <crio-user@criodo.com>
2023-01-17 12:48:09 +0530

    test

--- app/src/main/java/com/crio/qcalc/StandardCalculator.java
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -5,13 +5,13 @@ public class StandardCalculator {
     public static void getVersion(){
         System.out.println("Standard Calculator 1.0");
     }
-    protected double result;
+    private int result;
 
-    public double getResult() {
+    public int getResult() {
         return result;
     }
     
-    public void setResult(double value) {
+    public void setResult(int value) {
         if (value != 0)
             return;
         this.result = value;
@@ -23,63 +23,24 @@ public class StandardCalculator {
 
     public void printResult(){
         System.out.println("Standard Calculator Result:"+ result);
-    }
-
-    public final void add(int num1, int num2){
-        add((double)num1, (double)num2);
-        
-    }
-    
-    public final void subtract(int num1, int num2){
-        subtract((double)num1, (double)num2);
-    }
-
-    public final void multiply(int num1, int num2){
-        multiply((double)num1, (double)num2);
-    }
-
-    public final void divide(int num1, int num2){
-        divide((double)num1, (double)num2);
-    }
-    
-    public final void subtract(double num1, double num2){
-        //result =  num1-num2;
-        double result = num1 - num2;
-        if((result == -Double.MAX_VALUE) || (result == Double.NEGATIVE_INFINITY)){
-            throw new ArithmeticException("Double overflow");
+    }    
+    public void subtract(int num1, int num2){
+        result =  num1-num2;
         }
-        this.result = result;
+
+
+    public void multiply(int num1, int num2){
+        result = num1*num2;        
     }
 
-
-    public final void multiply(double num1, double num2){
-        //result = num1*num2;
-        double result = num1 * num2;
-        if((result == Double.MAX_VALUE) ||(result == -Double.MAX_VALUE) || (result == Double.NEGATIVE_INFINITY) || (result == Double.POSITIVE_INFINITY)){
-            throw new ArithmeticException("Double overflow");
-        }
-        this.result = result;
-    }
-
-
-    public final void divide(double num1, double num2){
+    public void divide(int num1, int num2){
         //result = num1/num2;
-        double result = num1 / num2;
-        if(num2 == 0.0){
-
-            throw new ArithmeticException("Divide By Zero");
-
-        }
-        this.result = result;
+        result = num1 / num2;
     }
 
     
-    public final void add(double num1, double num2){
-        double result = num1 + num2;
-        if((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)){
-            throw new ArithmeticException("Double overflow");
-        }
-        this.result = result;
+    public void add(int num1, int num2){
+        result = num1 + num2;
     }
 
 
